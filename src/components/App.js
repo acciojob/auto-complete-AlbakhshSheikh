ddf50d4d7e06d8f89ce4bec25543eca9e76dfee6
Albakhsh Sheikh <98633969+AlbakhshSheikh@users.noreply.github.com>
2025-06-03 23:36:27 +0530

    run

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './../styles/App.css';
-import Autocomplete from "./AutoComplete"
+import Autocomplete from "./Autocomplete"
 
 const App = () => {
   let fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
